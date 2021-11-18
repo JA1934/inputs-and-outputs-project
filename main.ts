8a379c966a6a065f586a96f48fa158e73ff5a6cc
JA1934 <92798990+JA1934@users.noreply.github.com>
2021-11-18 09:31:43 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,11 @@ let value = 0
 basic.forever(function () {
     value = pins.analogReadPin(AnalogPin.P0)
     basic.showNumber(value)
-    if (pins.analogReadPin(AnalogPin.P0) > 200) {
+    if (value > 300) {
         pins.digitalWritePin(DigitalPin.P1, 1)
     } else {
         pins.digitalWritePin(DigitalPin.P1, 1)
-        basic.pause(2000)
-        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
     }
 })
